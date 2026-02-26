fe0218c8e160a24568282a1c349b7c43f9d9bc6f
cas7breeder <castbreeder@posteo.net>
2026-02-26 23:58:38 +0200

    neovim: show and highlight trailing whitespace, trim trailing line whitespace and trailing file newlines on saves, add a ReloadConfig function

--- .config/nvim/lua/mine/init.lua
+++ .config/nvim/lua/mine/init.lua
@@ -2,15 +2,47 @@ require("mine.remap")
 require("mine.set")
 require("mine.packer")
 
+vim.o.termguicolors = true
+vim.g.neon_style = "doom"
+vim.g.neon_transparent = true
+vim.cmd.colorscheme 'neon'
+vim.api.nvim_set_hl(0, "Whitespace", { fg = "#00ff00", bold = true })
+vim.api.nvim_set_hl(0, "SpecialKey", { fg = "#ff5555", bold = true })
+
+vim.opt.listchars = {
+  tab = "→ ",
+  trail = "·",
+  nbsp = "␣",
+}
+
+local group = vim.api.nvim_create_augroup("TrimWhitespace", { clear = true })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = group,
+  pattern = "*",
+  callback = function()
+    -- remove trailing whitespace
+    vim.cmd([[%s/\s\+$//e]])
+    -- remove extra blank lines at end of file
+    vim.cmd([[%s/\n\+\%$//e]])
+  end,
+})
+
+vim.api.nvim_create_user_command("ReloadConfig", function()
+  for name,_ in pairs(package.loaded) do
+    if name:match("^mine") then
+      package.loaded[name] = nil
+    end
+  end
+  vim.cmd("source $MYVIMRC")
+end, {})
+
+-- Other fine options for color schemes
 -- https://github.com/olivercederborg/poimandres.nvim
 --vim.cmd.colorscheme 'poimandres'
 
 -- https://github.com/rafamadriz/neon
 -- doom/dark/light
-vim.o.termguicolors = true
-vim.g.neon_style = "doom"
-vim.g.neon_transparent = true
-vim.cmd.colorscheme 'neon'
 
 -- https://github.com/marko-cerovac/material.nvim
 -- daker/lighter/oceanic/palenight/deep ocean/

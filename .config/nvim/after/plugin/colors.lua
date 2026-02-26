e04095a5316079440e6a8d0fd0d9b1d637905620
cas7breeder <castbreeder@posteo.net>
2026-02-27 00:40:56 +0200

    neovim: improve ux

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,2 +1,4 @@
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "Whitespace", { fg = "#00ff00", bold = true })
+vim.api.nvim_set_hl(0, "SpecialKey", { fg = "#ff5555", bold = true })

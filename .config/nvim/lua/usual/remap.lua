30722363de1ccbce10cd8c0d7e6e083925b59607
cas7breeder <castbreeder@posteo.net>
2026-04-15 15:05:17 +0300

    neovim: add more keybindings

--- .config/nvim/lua/usual/remap.lua
+++ .config/nvim/lua/usual/remap.lua
@@ -1,4 +1,13 @@
---  Write buffer vim.g.mapleader = " "
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<M-q>", "<cmd>q<CR>")
+vim.keymap.set("n", "<leader>s", "<cmd>w<CR>")
+vim.keymap.set("n", "<leader>v", "<cmd>vsp<CR>")
+vim.keymap.set("n", "<leader>h", "<cmd>sp<CR>")
+vim.keymap.set("n", "<leader>n", "<cmd>enew<CR>")
+vim.keymap.set("n", "<leader>c", "<C-w>x")
+
+vim.keymap.set("v", "<leader>j", "<cmd>'<,'>join<CR>")
 
 -- File view mode
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
@@ -15,6 +24,12 @@ vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- Reload config
 vim.keymap.set("n", "<leader>R", "<cmd>ReloadConfig<CR>")
+
+-- Improve split window navigation
+vim.keymap.set("n", "<A-h>", "<C-w>h")
+vim.keymap.set("n", "<A-j>", "<C-w>j")
+vim.keymap.set("n", "<A-k>", "<C-w>k")
+vim.keymap.set("n", "<A-l>", "<C-w>l")
 
 -- For tmux
 --vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")

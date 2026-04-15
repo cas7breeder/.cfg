81c0519331e876cb6474316a8866d5f619d8ec7f
cas7breeder <castbreeder@posteo.net>
2026-04-15 15:04:17 +0300

    neovim: clean-up vscode profile

--- .config/nvim/lua/my_vscode_config/set.lua
+++
@@ -1,8 +0,0 @@
--- TODO: review, check with VSCode??
---vim.opt.nu = true
---vim.opt.relativenumber = true
--- Editor customization (relative line number, scrolloff, etc) is handled by VSCode
-
--- VSCode will override these?
---vim.opt.hlsearch = false
---vim.opt.incsearch = true

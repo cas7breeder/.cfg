78f18ede78344f277146d5ef14fb5cb528605fff
cas7breeder <castbreeder@posteo.net>
2026-04-16 16:16:44 +0300

    neovim: remove visual artifact triggers when running in vscode profile

---
+++ .config/nvim/lua/my_vscode_config/set.lua
@@ -0,0 +1,7 @@
+vim.opt.lazyredraw = true
+vim.opt.wrap = false
+vim.opt.hlsearch = false
+vim.opt.relativenumber = false
+vim.opt.cursorline = false
+vim.opt.incsearch = false
+vim.opt.signcolumn = "no"
